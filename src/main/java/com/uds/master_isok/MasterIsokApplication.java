6e7c4e32ab0959b70ce7e23cd80dc5eda3444e13
Joel Proxi <joeledmond95@gmail.com>
2025-04-20 08:32:22 +0100

    (backend): initial commit

---
+++ src/main/java/com/uds/master_isok/MasterIsokApplication.java
@@ -0,0 +1,13 @@
+package com.uds.master_isok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterIsokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterIsokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uds/master_isok/MasterIsokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uds.master_isok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterIsokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
