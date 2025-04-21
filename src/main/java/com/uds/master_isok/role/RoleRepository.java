8961c1ec43c4651e03b0b27b2802a695f23ae0b0
Joel Proxi <joeledmond95@gmail.com>
2025-04-21 13:53:09 +0100

    feat:(backend): remove all files

--- src/main/java/com/uds/master_isok/role/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uds.master_isok.role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role> findByRoleName(Role.RoleType roleName);
-}
