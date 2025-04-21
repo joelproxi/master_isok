1ed62919d29cc4ebfe909b67796bc0a0b3ddd188
Joel Proxi <joeledmond95@gmail.com>
2025-04-21 21:57:42 +0100

    feat:(backend): add student request and response DTO

---
+++ src/main/java/com/uds/master_isok/student/StudentResponse.java
@@ -0,0 +1,14 @@
+package com.uds.master_isok.student;
+
+import java.time.Instant;
+
+public record StudentResponse(
+        Long studentId ,
+        String firstName ,
+        String lastName ,
+        String email,
+        String dateOfBirth,
+        Instant updatedAt,
+        Instant createdAt
+) {
+}
