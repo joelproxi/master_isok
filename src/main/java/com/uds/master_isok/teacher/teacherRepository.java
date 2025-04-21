a828bf330c8d6c1d79e0d38e973ecf2177ef40d3
Joel Proxi <joeledmond95@gmail.com>
2025-04-21 17:56:58 +0100

    feat:(backend): add repository

---
+++ src/main/java/com/uds/master_isok/teacher/teacherRepository.java
@@ -0,0 +1,7 @@
+package com.uds.master_isok.teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface teacherRepository extends JpaRepository<Teacher, Long> {
+
+}
